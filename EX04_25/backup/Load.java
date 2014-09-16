14a4697f772676b71f83b7a9db3818d6a90626c3
JaneWu <sheep800828@gmail.com>
2014-09-16 18:16:16 +0800

    add EX04_25 心電感應 project

---
+++ EX04_25/backup/Load.java
@@ -0,0 +1,79 @@
+package com.project.interfaces;
+
+import irdc.ex04_25.EX04_25;
+
+import java.io.File;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.os.Environment;
+import android.util.Log;
+
+import dalvik.system.DexClassLoader;
+
+public class Load
+{
+  String folderPath = "project";
+  String fileName = "";
+  
+  /**
+   * @param fileName
+   */
+  public Load(String fileName)
+  {
+    super();
+    this.fileName = fileName;
+  }
+  
+  @SuppressLint("NewApi")
+  public void loadJar(Context context)
+  {
+    try
+    {
+      String sourceFilePath = Environment.getExternalStorageDirectory()
+          + File.separator + folderPath + File.separator + fileName;
+      
+      // export jar path
+      File sourceFile = new File(sourceFilePath);
+      
+      // export dex tmp path
+      File file = context.getDir("osdk", 0);
+      
+      Log.e("path", sourceFile.getAbsolutePath());
+      
+      DexClassLoader classLoader = new DexClassLoader(
+          sourceFile.getAbsolutePath(), file.getAbsolutePath(), null,
+          context.getClassLoader());
+      
+      Class<?> libProviderClazz = classLoader
+          .loadClass("com.project.interfaces.InterfaceTest");
+      
+      // interface
+      MainInterface mMainInterface = (MainInterface) libProviderClazz
+          .newInstance();
+      
+      // return jar result
+      // String str = mMainInterface.sayHello();
+      // Toast.makeText(context, str, Toast.LENGTH_SHORT).show();
+      
+      //exe
+      mMainInterface.showResult();
+      
+      //return
+      EX04_25.mUserChoice = mMainInterface.ismUserChoice();
+      EX04_25.intTimes = mMainInterface.getIntTimes();
+      EX04_25.mMyChoice = mMainInterface.getmMyChoice();
+      
+
+      File deleteFile = new File(sourceFile.getAbsolutePath());
+      boolean deleted = deleteFile.delete();
+      Log.e("deleteFile", String.valueOf(deleted));
+      
+    }
+    catch (Exception e)
+    {
+      e.printStackTrace();
+    }
+  }
+  
+}
