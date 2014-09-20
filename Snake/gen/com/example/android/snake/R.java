cc5d49c5e112e428cf042ef65f86754ed0e68645
JaneWu <sheep800828@gmail.com>
2014-09-20 14:57:31 +0800

    add btn

--- Snake/gen/com/example/android/snake/R.java
+++ Snake/gen/com/example/android/snake/R.java
@@ -28,6 +28,10 @@ containing a value of this type.
         public static final int yellowstar2=0x7f020003;
     }
     public static final class id {
+        public static final int button1=0x7f060003;
+        public static final int button2=0x7f060002;
+        public static final int button3=0x7f060004;
+        public static final int button4=0x7f060005;
         public static final int snake=0x7f060000;
         public static final int text=0x7f060001;
     }
