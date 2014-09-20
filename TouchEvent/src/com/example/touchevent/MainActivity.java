a52b79e384411dc15009be245405a5e494ac78a1
JaneWu <sheep800828@gmail.com>
2014-09-20 16:09:49 +0800

    add ontouchevent

--- TouchEvent/src/com/example/touchevent/MainActivity.java
+++ TouchEvent/src/com/example/touchevent/MainActivity.java
@@ -4,109 +4,96 @@ import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsoluteLayout;
+import android.widget.AbsoluteLayout.LayoutParams;
+import android.widget.TextView;
 import android.os.Build;
 
 public class MainActivity extends ActionBarActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+	private float touchX;
+	private float touchY;
+	private int tvWidth = LayoutParams.WRAP_CONTENT;
+	private int tvHeight = LayoutParams.WRAP_CONTENT;
+	private TextView tv = null;
 
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
+		if (savedInstanceState == null) {
+			getSupportFragmentManager().beginTransaction()
+					.add(R.id.container, new Fragment()).commit();
+		}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+		// 取得 TextView 物件
+		tv = (TextView) findViewById(R.id.textWidget);
+		tv.setTextColor(0xFF000000);
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-    
-    public boolean onTouchEvent(MotionEvent event) {
-    	 
-        float X = event.getX(); // 觸控的 X 軸位置
-        float Y = event.getY(); // 觸控的 Y 軸位置
- 
-        switch (event.getAction()) { // 判斷觸控的動作
- 
-        case MotionEvent.ACTION_DOWN: // 按下
-            downX = event.getX();
-            downY = event.getY();
- 
-            return true;
-        case MotionEvent.ACTION_MOVE: // 拖曳
- 
-            return true;
-        case MotionEvent.ACTION_UP: // 放開
-            Log.d("onTouchEvent-ACTION_UP","UP");
-            upX = event.getX();
-            upY = event.getY();
-            float x=Math.abs(upX-downX);
-            float y=Math.abs(upY-downY);
-            double z=Math.sqrt(x*x+y*y);
-            int jiaodu=Math.round((float)(Math.asin(y/z)/Math.PI*180));//角度
-             
-            if (upY < downY && jiaodu>45) {//上
-                Log.d("onTouchEvent-ACTION_UP","角度:"+jiaodu+", 動作:上");
-            }else if(upY > downY && jiaodu>45) {//下
-                Log.d("onTouchEvent-ACTION_UP","角度:"+jiaodu+", 動作:下");
-            }else if(upX < downX && jiaodu< =45) {//左
-                Log.d("onTouchEvent-ACTION_UP","角度:"+jiaodu+", 動作:左");
-                // 原方向不是向右時，方向轉右
-                if (mDirection != EAST) {
-                    mNextDirection = WEST;
-                }
-            }else if(upX > downX && jiaodu< =45) {//右
-                Log.d("onTouchEvent-ACTION_UP","角度:"+jiaodu+", 動作:右");
-                // 原方向不是向左時，方向轉右
-                if (mDirection ! = WEST) {
-                    mNextDirection = EAST;
-                }
-            }
-            return true;
-        }
- 
-        return super.onTouchEvent(event);
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 
-        public PlaceholderFragment() {
-        }
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
-    }
+	@Override
+	// 利用 MotionEvent 處理觸控程序
+	public boolean onTouchEvent(MotionEvent event) {
+
+		touchX = event.getX(); // 觸控的 X 軸位置
+		touchY = event.getY() - 50; // 觸控的 Y 軸位置
+
+		// 判斷觸控動作
+		switch (event.getAction()) {
+
+		case MotionEvent.ACTION_DOWN: // 按下
+
+			// 設定 TextView 內容, 大小, 位置
+			tv.setText("X: " + touchX + ", Y: " + touchY + ", 按下");
+			tv.setLayoutParams(new AbsoluteLayout.LayoutParams(tvWidth,
+					tvHeight, (int) touchX, (int) touchY));
+			break;
+
+		case MotionEvent.ACTION_MOVE: // 拖曳移動
+
+			// 設定 TextView 內容, 大小, 位置
+			tv.setText("X: " + touchX + ", Y: " + touchY + ", 拖曳移動");
+			tv.setLayoutParams(new AbsoluteLayout.LayoutParams(tvWidth,
+					tvHeight, (int) touchX, (int) touchY));
+			break;
+
+		case MotionEvent.ACTION_UP: // 放開
+
+			// 設定 TextView 內容
+			tv.setText("X: " + touchX + ", Y: " + touchY + ", 放開");
+			break;
+		}
+
+		// TODO Auto-generated method stub
+		return super.onTouchEvent(event);
+	}
 
 }
