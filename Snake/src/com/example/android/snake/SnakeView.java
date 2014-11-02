c15b09e672b2349ae8728bfb5b851ddc762a07ff
JaneWu <sheep800828@gmail.com>
2014-11-02 12:39:36 +0800

    mod SnakeView.java

--- Snake/src/com/example/android/snake/SnakeView.java
+++ Snake/src/com/example/android/snake/SnakeView.java
@@ -27,8 +27,11 @@ import android.util.AttributeSet;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.AbsoluteLayout;
 import android.widget.TextView;
+import android.widget.AbsoluteLayout.LayoutParams;
 
 /**
  * SnakeView: implementation of a simple game of Snake
@@ -36,6 +39,49 @@ import android.widget.TextView;
  * 
  */
 public class SnakeView extends TileView {
+	//touch event varibate
+	private float touchX;
+	private float touchY;
+	private int tvWidth = LayoutParams.WRAP_CONTENT;
+	private int tvHeight = LayoutParams.WRAP_CONTENT;
+//	private TextView tv = null;
+	
+	@Override
+	// 利用 MotionEvent 處理觸控程序
+	public boolean onTouchEvent(MotionEvent event) {
+
+		touchX = event.getX(); // 觸控的 X 軸位置
+		touchY = event.getY() - 50; // 觸控的 Y 軸位置
+
+		// 判斷觸控動作
+		switch (event.getAction()) {
+
+		case MotionEvent.ACTION_DOWN: // 按下
+
+			// 設定 TextView 內容, 大小, 位置
+			tv.setText("X: " + touchX + ", Y: " + touchY + ", 按下");
+			tv.setLayoutParams(new AbsoluteLayout.LayoutParams(tvWidth,
+					tvHeight, (int) touchX, (int) touchY));
+			break;
+
+		case MotionEvent.ACTION_MOVE: // 拖曳移動
+
+			// 設定 TextView 內容, 大小, 位置
+			tv.setText("X: " + touchX + ", Y: " + touchY + ", 拖曳移動");
+			tv.setLayoutParams(new AbsoluteLayout.LayoutParams(tvWidth,
+					tvHeight, (int) touchX, (int) touchY));
+			break;
+
+		case MotionEvent.ACTION_UP: // 放開
+
+			// 設定 TextView 內容
+			tv.setText("X: " + touchX + ", Y: " + touchY + ", 放開");
+			break;
+		}
+
+		// TODO Auto-generated method stub
+		return super.onTouchEvent(event);
+	}
 
     private static final String TAG = "SnakeView";
 
