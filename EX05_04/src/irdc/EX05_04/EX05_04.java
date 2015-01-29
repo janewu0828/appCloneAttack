fce8150fafa3032a5edd1b806e15a4dbb9c2e198
JaneWu <sheep800828@gmail.com>
2015-01-30 03:11:13 +0800

    update notes

--- EX05_04/src/irdc/EX05_04/EX05_04.java
+++ EX05_04/src/irdc/EX05_04/EX05_04.java
@@ -6,7 +6,9 @@ import static com.project.module.ProjectConfig.checkConnection;
 import static com.project.module.ProjectConfig.mAppContext;
 import static com.project.module.ProjectConfig.mContext;
 import static com.project.module.ProjectConfig.showCheckuserError;
+
 import com.project.module.SendPostRunnable;
+
 import android.app.Activity;
 /*必需引用content.Intent類別來開啟email client*/
 import android.content.Intent;
@@ -38,13 +40,13 @@ public class EX05_04 extends Activity
     super.onCreate(savedInstanceState);
     setContentView(R.layout.main2);
 
-    // 取得getApplicationContext()資源
+    // get global Application object of the current process
     mAppContext = getApplicationContext();
-    // 顯示AlertDialog所需的資源
+    // get context for AlertDialog
     mContext = EX05_04.this;
-    // 顯示第一次驗證成功訊息
+    // show a message of authentication is successful in first time
     isShowTxt = true;
-    // 檢查網路組態設置
+    // check network setting on device
     checkConnection();
 
     /* 透過findViewById建構子來建構Button物件 */
@@ -72,18 +74,17 @@ public class EX05_04 extends Activity
       public void onClick(View v)
       {
         // TODO Auto-generated method stub
-
-        // check user , download file and dynamic loading-----
+        // ---check user, download file and dynamic loading---
         SendPostRunnable sr = new SendPostRunnable(fileName,
             getApplicationContext());
 
-        // 啟動一個Thread(執行緒)，將要傳送的資料放進Runnable中，讓Thread執行
+        // start a Thread, the data to be transferred into the Runnable, so that Thread execute
         Thread t = new Thread(sr);
         t.start();
 
         try
         {
-          // wait thread t
+          // wait Thread t
           t.join();
         } catch (InterruptedException e)
         {
@@ -97,11 +98,12 @@ public class EX05_04 extends Activity
             Toast.makeText(getApplicationContext(),
                 getResources().getString(R.string.toast_checkuser_true),
                 Toast.LENGTH_SHORT).show();
-          // 第一次顯示驗證成功訊息後，不再顯示
+          // show a message of Authentication is successful in first time
           isShowTxt = false;
 
         } else
         {
+          // show a Alert Dialog that Authentication is failed
           showCheckuserError();
         }
       }
