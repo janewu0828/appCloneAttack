cd8d7fb1a06ed4bd82452092b7d070d1dd38447d
JaneWu <sheep800828@gmail.com>
2015-01-30 02:36:02 +0800

    update notes

--- EX04_24/src/com/project/module/ProjectConfig.java
+++ EX04_24/src/com/project/module/ProjectConfig.java
@@ -3,44 +3,43 @@ package com.project.module;
 import irdc.ex04_24.R;
 import android.content.Context;
 
-public class ProjectConfig
-{
-  // 取得getApplicationContext()資源
-  public static Context mAppContext;
+public class ProjectConfig {
+	// get global Application object of the current process
+	public static Context mAppContext;
 
-  // 顯示AlertDialog所需的資源
-  public static Context mContext;
+	// get context for AlertDialog
+	public static Context mContext;
 
-  // 顯示提醒視窗
-  public static AlertDialogManager alert = new AlertDialogManager();
+	// show AlertDialog
+	public static AlertDialogManager alert = new AlertDialogManager();
 
-  // 用於第一次，是否顯示驗證成功訊息
-  public static boolean isShowTxt;
+	// show a message of authentication is successful in first time
+	public static boolean isShowTxt;
 
-  // 載入部分檔案的完整檔名
-  public static String fileName = "output0420.jar";
+	// ---dynamic loading file(JAR)---
+	public static String fileName = "output0420.jar";
 
-  public static void checkConnection()
-  {
-    // TODO Auto-generated method stub
-    // 檢查網路組態設置
-    ConnectionDetector cd = new ConnectionDetector(mAppContext);
-    if (!cd.isConnectingToInternet())
-    {
-      alert.showAlertDialog(mContext,
-          mContext.getString(R.string.alert_internet_error_title),
-          mContext.getString(R.string.alert_internet_error_msg), false);
-    }
-  }
+	public static void checkConnection() {
+		// TODO Auto-generated method stub
+		// check network setting on device
+		ConnectionDetector cd = new ConnectionDetector(mAppContext);
+		if (!cd.isConnectingToInternet()) {
+			alert.showAlertDialog(mContext,
+					mContext.getString(R.string.alert_internet_error_title),
+					mContext.getString(R.string.alert_internet_error_msg),
+					false);
+		}
+	}
 
-  public static void showCheckuserError()
-  {
-    // TODO Auto-generated method stub
-    //顯示非法user提醒視窗
-    alert.showAlertDialog(mContext,
-        mContext.getResources().getString(R.string.alert_checkuser_error_title),
-        mContext.getResources().getString(R.string.alert_checkuser_error_msg),
-        false);
-  }
+	public static void showCheckuserError() {
+		// TODO Auto-generated method stub
+		// show a Alert Dialog that Authentication is failed
+		alert.showAlertDialog(
+				mContext,
+				mContext.getResources().getString(
+						R.string.alert_checkuser_error_title),
+				mContext.getResources().getString(
+						R.string.alert_checkuser_error_msg), false);
+	}
 
 }
