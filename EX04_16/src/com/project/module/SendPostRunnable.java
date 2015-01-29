10430e2fba615eeafda9e307e41d809733910808
JaneWu <sheep800828@gmail.com>
2015-01-30 00:40:40 +0800

    update getAppId() and notes

--- EX04_16/src/com/project/module/SendPostRunnable.java
+++ EX04_16/src/com/project/module/SendPostRunnable.java
@@ -18,216 +18,214 @@ import android.os.Environment;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 
-public class SendPostRunnable implements Runnable
-{
-  Context appContext;
+public class SendPostRunnable implements Runnable {
 
-  String fileName = "";
+	Context appContext;
 
-  String appid = null;
-  String deviceid = null;
-  String IMEI = null;
+	String fileName = "";
 
-  boolean result = true;
+	String appid = null;
+	String deviceid = null;
+	String IMEI = null;
 
-  /**
-   * @param result
-   */
-  public SendPostRunnable(String fileName, Context appContext)
-  {
-    super();
-    this.fileName = fileName;
-    this.appContext = appContext;
-    this.appid = getAppId();
-    this.deviceid = getDeviceId(appContext);
-    this.IMEI = getIMEI(appContext);
-  }
+	boolean result = true;
 
-  private void sendPostDataToInternet()
-  {
-    CheckUser cu = new CheckUser(appid, deviceid, IMEI);
-    setResult(cu.checkUser());
-    Log.i("cu.checkUser()", "" + result);
+	// download file from server
+	String file_url = "http://140.118.109.165/web2/download/" + fileName;
+	String outputFilePath = Environment.getExternalStorageDirectory()
+			.getAbsolutePath() + "/project";
 
-    if (result)
-    {
-      // Asnyc Dowload
-      String file_url = "http://140.118.109.165/web2/download/"
-          + fileName;
+	/**
+	 * @param result
+	 */
+	public SendPostRunnable(String fileName, Context appContext) {
+		super();
+		this.fileName = fileName;
+		this.appContext = appContext;
+		this.appid = getAppId();
+		this.deviceid = getDeviceId(appContext);
+		this.IMEI = getIMEI(appContext);
+	}
 
-      new DownloadFileFromURL().execute(file_url);
-    }
-  }
+	private void sendPostDataToInternet() {
+		CheckUser cu = new CheckUser(appid, deviceid, IMEI);
+		setResult(cu.checkUser());
+		Log.i("cu.checkUser()", "" + result);
 
-  @Override
-  public void run()
-  {
-    // TODO Auto-generated method stub
-    sendPostDataToInternet();
+		if (result) {
+			// Asnyc Dowload
+			new DownloadFileFromURL().execute(file_url);
+		}
+	}
 
-  }
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		sendPostDataToInternet();
+	}
 
-  public void setResult(boolean result)
-  {
-    // TODO Auto-generated method stub
-    this.result = result;
-  }
+	public void setResult(boolean result) {
+		// TODO Auto-generated method stub
+		this.result = result;
+	}
 
-  public boolean getResult()
-  {
-    // TODO Auto-generated method stub
-    return result;
-  }
+	public boolean getResult() {
+		// TODO Auto-generated method stub
+		return result;
+	}
 
-  // AsynTask
-  class DownloadFileFromURL extends AsyncTask<String, String, String>
-  {
+	// AsynTask
+	class DownloadFileFromURL extends AsyncTask<String, String, String> {
 
-    /**
-     * Before starting background thread Show Progress Bar Dialog
-     * */
-    @Override
-    protected void onPreExecute()
-    {
-      super.onPreExecute();
-    }
+		/**
+		 * Before starting background thread Show Progress Bar Dialog
+		 * */
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
+		}
 
-    /**
-     * Downloading file in background thread
-     * */
-    @Override
-    protected String doInBackground(String... f_url)
-    {
-      int count;
-      try
-      {
-        URL url = new URL(f_url[0]);
-        URLConnection conection = url.openConnection();
-        conection.connect();
+		/**
+		 * Downloading file in background thread
+		 * */
+		@Override
+		protected String doInBackground(String... f_url) {
+			int count;
+			try {
+				URL url = new URL(f_url[0]);
+				URLConnection conection = url.openConnection();
+				conection.connect();
 
-        // this will be useful so that you can show a tipical 0-100%
-        // progress bar
-        int lenghtOfFile = conection.getContentLength();
+				// this will be useful so that you can show a tipical 0-100%
+				// progress bar
+				int lenghtOfFile = conection.getContentLength();
 
-        // download the file
-        InputStream input = new BufferedInputStream(url.openStream(),
-            8192);
+				// download the file
+				InputStream input = new BufferedInputStream(url.openStream(),
+						8192);
 
-        File outputPath = new File(Environment
-            .getExternalStorageDirectory().getAbsolutePath()
-            + "/project");
+				File outputFile = new File(outputFilePath);
 
-        if (!outputPath.exists())
-        {
-          outputPath.mkdir();
-        }
+				if (!outputFile.exists()) {
+					outputFile.mkdir();
+				}
+				// Log.e("e", outputFile.toString());
 
-        Log.e("e", outputPath.toString());
+				// Output stream
+				OutputStream output = new FileOutputStream(outputFilePath + "/"
+						+ fileName);
 
-        // Output stream
-        OutputStream output = new FileOutputStream(Environment
-            .getExternalStorageDirectory().toString()
-            + "/project/"
-            + fileName);
+				byte data[] = new byte[2048];
 
-        byte data[] = new byte[2048];
+				long total = 0;
 
-        long total = 0;
+				while ((count = input.read(data)) != -1) {
+					total += count;
+					// publishing the progress....
+					// After this onProgressUpdate will be called
+					publishProgress("" + (int) ((total * 100) / lenghtOfFile));
 
-        while ((count = input.read(data)) != -1)
-        {
-          total += count;
-          // publishing the progress....
-          // After this onProgressUpdate will be called
-          publishProgress("" + (int) ((total * 100) / lenghtOfFile));
+					// writing data to file
+					output.write(data, 0, count);
+				}
 
-          // writing data to file
-          output.write(data, 0, count);
-        }
+				// flushing output
+				output.flush();
 
-        // flushing output
-        output.flush();
+				// closing streams
+				output.close();
+				input.close();
 
-        // closing streams
-        output.close();
-        input.close();
+			} catch (Exception e) {
+				Log.e("Error: ", e.getMessage());
+			}
 
-      } catch (Exception e)
-      {
-        Log.e("Error: ", e.getMessage());
-      }
+			return null;
+		}
 
-      return null;
-    }
+		/**
+		 * Updating progress bar
+		 * */
+		protected void onProgressUpdate(String... progress) {
+			// setting progress percentage
+		}
 
-    /**
-     * Updating progress bar
-     * */
-    protected void onProgressUpdate(String... progress)
-    {
-      // setting progress percentage
-    }
+		/**
+		 * After completing background task Dismiss the progress dialog
+		 * **/
+		@Override
+		protected void onPostExecute(String file_url) {
+			// dynamic loading -----
+			Load ld = new Load(fileName);
+			ld.loadJar(appContext);
+		}
 
-    /**
-     * After completing background task Dismiss the progress dialog
-     * **/
-    @Override
-    protected void onPostExecute(String file_url)
-    {
-      // dynamic loading -----
-      Load ld = new Load(fileName);
-      ld.loadJar(appContext);
-    }
+	}
 
-  }
+	private String getAppId() {
+		String PACKAGE_NAME = appContext.getPackageName();
+		// Log.i("package name", PACKAGE_NAME);
 
-  private String getAppId()
-  {
-    String PACKAGE_NAME = appContext.getPackageName();
-    Log.i("package name", PACKAGE_NAME);
+		// ---get hash code of apk---
+		String apkName = "";
+		String apkPath = Environment.getDataDirectory() + "/app/" + apkName;
+		File apkFile = null;
+		int i = 0;
+		int isNext = 0;
 
-    try
-    {
-      appid = AeSimpleSHA1.SHA1(PACKAGE_NAME);
-    } catch (NoSuchAlgorithmException e)
-    {
-      // TODO 自動產生的 catch 區塊
-      e.printStackTrace();
-    } catch (UnsupportedEncodingException e)
-    {
-      // TODO 自動產生的 catch 區塊
-      e.printStackTrace();
-    }
+		// ---get apk file name---
+		for (i = 1; isNext < 1; i++) {
+			apkName = PACKAGE_NAME + "-" + i + ".apk";
+			// System.out.println("fileName= " + fileName);
 
-    Log.i("appId", appid);
-    Log.i("appId length", "" + appid.length());
+			apkFile = new File(apkPath);
+			// System.out.println("path= " + apkFile.getAbsolutePath());
 
-    return appid;
-  }
+			if (!apkFile.exists()) {
+				System.out.println("no exists= " + apkFile.toString());
 
-  private String getDeviceId(Context context2)
-  {
-    // TODO Auto-generated method stub
-    DeviceUuidFactory DFactory = new DeviceUuidFactory(context2);
-    String deviceId = DFactory.getDeviceUuid().toString();
+			} else {
+				System.out.println("exists= " + apkFile.toString());
+				isNext++;
+			}
+		}
 
-    Log.i("deviceId", deviceId);
-    Log.i("deviceId" + " length", "" + deviceId.length());
+		// ---get hash code of apk---
+		try {
+			appid = Hash.sha256(apkFile);
+			System.out.println("appid= " + appid);
+			System.out.println("appId length= " + appid.length());
 
-    return deviceId;
-  }
+		} catch (Exception e) {
+			// TODO 自動產生的 catch 區塊
+			e.printStackTrace();
+			System.out.println("Error: " + e.getMessage());
 
-  private String getIMEI(Context context2)
-  {
-    // TODO Auto-generated method stub
-    TelephonyManager tM = (TelephonyManager) appContext
-        .getSystemService(Context.TELEPHONY_SERVICE);
-    String imei = tM.getDeviceId();
-    
-    Log.i("imei", imei);
-    Log.i("imei" + " length", "" + imei.length());
+		}
+		return appid;
+	}
 
-    return imei;
-  }
+	private String getDeviceId(Context context2) {
+		// TODO Auto-generated method stub
+		DeviceUuidFactory DFactory = new DeviceUuidFactory(context2);
+		String deviceId = DFactory.getDeviceUuid().toString();
+
+		Log.i("deviceId", deviceId);
+		Log.i("deviceId" + " length", "" + deviceId.length());
+
+		return deviceId;
+	}
+
+	private String getIMEI(Context context2) {
+		// TODO Auto-generated method stub
+		TelephonyManager tM = (TelephonyManager) appContext
+				.getSystemService(Context.TELEPHONY_SERVICE);
+		String imei = tM.getDeviceId();
+
+		Log.i("imei", imei);
+		Log.i("imei" + " length", "" + imei.length());
+
+		return imei;
+	}
 
 }
