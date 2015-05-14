d41295acbb93916ea203016be57a3e9d631ee927
jane0828 <janewu0828@gmail.com>
2015-05-14 10:56:09 +0800

    move to client_android folder

---
+++ client_android/EX05_04/src/com/project/module/ProjectConfig.java
@@ -0,0 +1,48 @@
+package com.project.module;
+
+import irdc.EX05_04.R;
+import android.content.Context;
+
+public class ProjectConfig
+{
+  // get global Application object of the current process
+  public static Context mAppContext;
+
+  // get context for AlertDialog
+  public static Context mContext;
+
+  // show AlertDialog
+  public static AlertDialogManager alert = new AlertDialogManager();
+
+  // show a message of authentication is successful in first time
+  public static boolean isShowTxt;
+
+  // ---dynamic loading file(JAR)---
+  public static String fileName = "outputEmail20140701.jar";
+
+  public static void checkConnection()
+  {
+    // TODO Auto-generated method stub
+    // check network setting on device
+    ConnectionDetector cd = new ConnectionDetector(mAppContext);
+    if (!cd.isConnectingToInternet())
+    {
+      alert.showAlertDialog(mContext,
+          mContext.getString(R.string.alert_internet_error_title),
+          mContext.getString(R.string.alert_internet_error_msg), false);
+    }
+  }
+
+  public static void showCheckuserError()
+  {
+    // TODO Auto-generated method stub
+    // show a Alert Dialog that Authentication is failed
+    alert
+        .showAlertDialog(
+            mContext,
+            mContext.getResources().getString(
+                R.string.alert_checkuser_error_title), mContext.getResources()
+                .getString(R.string.alert_checkuser_error_msg), false);
+  }
+
+}
