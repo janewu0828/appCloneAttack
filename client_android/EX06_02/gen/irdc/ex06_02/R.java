0d202895b9dce443b88ed41a2963848f5dd6026d
jane0828 <janewu0828@gmail.com>
2015-05-15 03:14:38 +0800

    add

--- client_android/EX06_02/gen/irdc/ex06_02/R.java
+++ client_android/EX06_02/gen/irdc/ex06_02/R.java
@@ -11,14 +11,13 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int black=0x7f020005;
-        public static final int blue=0x7f020007;
+        public static final int black=0x7f020004;
+        public static final int blue=0x7f020006;
         public static final int fail=0x7f020000;
         public static final int ic_launcher=0x7f020001;
         public static final int icon=0x7f020002;
-        public static final int project_icon=0x7f020003;
-        public static final int success=0x7f020004;
-        public static final int white=0x7f020006;
+        public static final int success=0x7f020003;
+        public static final int white=0x7f020005;
     }
     public static final class id {
         public static final int layout1=0x7f060000;
@@ -33,7 +32,10 @@ public final class R {
         public static final int alert_checkuser_error_title=0x7f040009;
         public static final int alert_internet_error_msg=0x7f040007;
         public static final int alert_internet_error_title=0x7f040006;
+        public static final int alert_personal_key_error_msg=0x7f04000c;
+        public static final int alert_personal_key_error_title=0x7f04000b;
         public static final int app_name=0x7f040000;
+        public static final int loading_msg=0x7f04000d;
         public static final int str_button1=0x7f040002;
         public static final int str_button2=0x7f040003;
         public static final int str_dialog_body=0x7f040005;
