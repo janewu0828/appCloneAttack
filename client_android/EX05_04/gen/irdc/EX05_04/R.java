b59e094bcc24c20320b3ad5205394d6fd53b4348
jane0828 <janewu0828@gmail.com>
2015-05-15 03:14:09 +0800

    add

--- client_android/EX05_04/gen/irdc/EX05_04/R.java
+++ client_android/EX05_04/gen/irdc/EX05_04/R.java
@@ -11,14 +11,13 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int black=0x7f020007;
-        public static final int blue=0x7f020006;
-        public static final int darkgray=0x7f020004;
+        public static final int black=0x7f020006;
+        public static final int blue=0x7f020005;
+        public static final int darkgray=0x7f020003;
         public static final int fail=0x7f020000;
         public static final int icon=0x7f020001;
-        public static final int project_icon=0x7f020002;
-        public static final int success=0x7f020003;
-        public static final int white=0x7f020005;
+        public static final int success=0x7f020002;
+        public static final int white=0x7f020004;
     }
     public static final class id {
         public static final int button1=0x7f06000e;
@@ -42,22 +41,25 @@ public final class R {
         public static final int main2=0x7f030001;
     }
     public static final class string {
-        public static final int alert_checkuser_error_msg=0x7f04000b;
-        public static final int alert_checkuser_error_title=0x7f04000a;
-        public static final int alert_internet_error_msg=0x7f040008;
-        public static final int alert_internet_error_title=0x7f040007;
+        public static final int alert_checkuser_error_msg=0x7f04000f;
+        public static final int alert_checkuser_error_title=0x7f04000e;
+        public static final int alert_internet_error_msg=0x7f04000c;
+        public static final int alert_internet_error_title=0x7f04000b;
+        public static final int alert_personal_key_error_msg=0x7f040011;
+        public static final int alert_personal_key_error_title=0x7f040010;
         public static final int app_name=0x7f040000;
+        public static final int loading_msg=0x7f040012;
         public static final int str_button=0x7f040001;
         public static final int str_button_cancel=0x7f040002;
         public static final int str_cc=0x7f040005;
-        public static final int str_cc_hint=0x7f04000d;
+        public static final int str_cc_hint=0x7f040008;
         public static final int str_message=0x7f040003;
-        public static final int str_message_hint=0x7f04000f;
+        public static final int str_message_hint=0x7f04000a;
         public static final int str_receive=0x7f040004;
-        public static final int str_receive_hint=0x7f04000c;
+        public static final int str_receive_hint=0x7f040007;
         public static final int str_subject=0x7f040006;
-        public static final int str_subject_hint=0x7f04000e;
-        public static final int toast_checkuser_true=0x7f040009;
+        public static final int str_subject_hint=0x7f040009;
+        public static final int toast_checkuser_true=0x7f04000d;
     }
     public static final class style {
         /** 
