f8a33b72f1fd541e3838de396d1c32f10dc40b4f
jane0828 <janewu0828@gmail.com>
2015-05-15 02:18:37 +0800

    add ACAPD

--- client_android/EX06_08/src/irdc/ex06_08/EX06_08.java
+++ client_android/EX06_08/src/irdc/ex06_08/EX06_08.java
@@ -1,24 +1,21 @@
 package irdc.ex06_08;
 
-import static com.project.module.ProjectConfig.mAppContext;
-import static com.project.module.ProjectConfig.mContext;
-import static com.project.module.ProjectConfig.isShowTxt;
-import static com.project.module.ProjectConfig.fileName;
-import static com.project.module.ProjectConfig.checkConnection;
-import static com.project.module.ProjectConfig.showCheckuserError;
-
-import com.project.module.SendPostRunnable;
-
+import static trustedappframework.subprojecttwo.module.ProjectConfig.mAppContext;
+import static trustedappframework.subprojecttwo.module.ProjectConfig.mContext;
+import static trustedappframework.subprojecttwo.module.ProjectConfig.class_separation_segment;
+import static trustedappframework.subprojecttwo.module.ProjectConfig.personal_key;
+import trustedappframework.subprojecttwo.module.ACAPD;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class EX06_08 extends Activity
 {
+  private ACAPD myACAPD;
+
   private Button myButton;
   public static ProgressBar myProgressBar;
   public static TextView myTextView;
@@ -34,10 +31,6 @@ public class EX06_08 extends Activity
     mAppContext = getApplicationContext();
     // get context for AlertDialog
     mContext = EX06_08.this;
-    // show a message of authentication is successful in first time
-    isShowTxt = true;
-    // check network setting on device
-    checkConnection();
 
     myButton = (Button) findViewById(R.id.myButton);
     myProgressBar = (ProgressBar) findViewById(R.id.myProgressBar);
@@ -49,42 +42,7 @@ public class EX06_08 extends Activity
       @Override
       public void onClick(View arg0)
       {
-
-        // check user , download file and dynamic loading-----
-        SendPostRunnable sr = new SendPostRunnable(fileName,
-            getApplicationContext());
-
-        // start a Thread, the data to be transferred into the Runnable, so that
-        // Thread execute
-        Thread t = new Thread(sr);
-        t.start();
-
-        try
-        {
-          // wait Thread t
-          t.join();
-        } catch (InterruptedException e)
-        {
-          // TODO Auto-generated catch block
-          e.printStackTrace();
-        }
-
-        if (sr.getResult())
-        {
-          if (isShowTxt)
-            Toast.makeText(
-                mContext,
-                mContext.getResources().getString(
-                    R.string.toast_checkuser_true),
-                Toast.LENGTH_SHORT).show();
-          // show a message of Authentication is successful in first time
-          isShowTxt = false;
-
-        } else
-        {
-          // show a Alert Dialog that Authentication is failed
-          showCheckuserError();
-        }
+        myACAPD = new ACAPD(class_separation_segment, personal_key);
       }
 
     });
