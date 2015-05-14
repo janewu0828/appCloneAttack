e0ad6c744ed2b7766ce277bae4a3c5957d70ed15
jane0828 <janewu0828@gmail.com>
2015-05-14 23:52:49 +0800

    add App Clone Attack Prevention and Detection (ACAPD)

--- client_android/dynamicLoading/src/irdc/ex04_16/EX04_16.java
+++ client_android/dynamicLoading/src/irdc/ex04_16/EX04_16.java
@@ -1,42 +1,29 @@
 package irdc.ex04_16;
 
-import static trustedappframework.subprojecttwo.module.ProjectConfig.checkConnection;
-import static trustedappframework.subprojecttwo.module.ProjectConfig.fileName;
-import static trustedappframework.subprojecttwo.module.ProjectConfig.isShowTxt;
-import static trustedappframework.subprojecttwo.module.ProjectConfig.loadFileName;
 import static trustedappframework.subprojecttwo.module.ProjectConfig.mAppContext;
 import static trustedappframework.subprojecttwo.module.ProjectConfig.mContext;
+import static trustedappframework.subprojecttwo.module.ProjectConfig.class_separation_segment;
+import static trustedappframework.subprojecttwo.module.ProjectConfig.class_separation_segment2;
+import static trustedappframework.subprojecttwo.module.ProjectConfig.class_separation_segment3;
 import static trustedappframework.subprojecttwo.module.ProjectConfig.personal_key;
-import static trustedappframework.subprojecttwo.module.ProjectConfig.showCheckUserError;
-import static trustedappframework.subprojecttwo.module.ProjectConfig.showPersonalKey;
-import static trustedappframework.subprojecttwo.module.ProjectConfig.showPersonalKeyError;
-import static trustedappframework.subprojecttwo.module.SendPostRunnable.outputFilePath;
-
-import java.io.File;
-
-
-
-
-
-
-
-import trustedappframework.subprojecttwo.interfaces.Load;
-import trustedappframework.subprojecttwo.module.Decrypt;
-import trustedappframework.subprojecttwo.module.PersonalKeyManager;
-import trustedappframework.subprojecttwo.module.SendPostRunnable;
+import static trustedappframework.subprojecttwo.module.ProjectConfig.personal_key2;
+import static trustedappframework.subprojecttwo.module.ProjectConfig.personal_key3;
+import trustedappframework.subprojecttwo.module.ACAPD;
 /* import相關class */
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 import android.util.Log;
 import android.view.View;
 
 public class EX04_16 extends Activity {
 	private static final String TAG = "EX04_16";
+
+	private ACAPD myACAPD;
 
 	public static Drawable d01;
 	public static Drawable d02;
@@ -72,12 +59,10 @@ public class EX04_16 extends Activity {
 		mAppContext = EX04_16.this.getApplicationContext();
 		// get context for AlertDialog
 		mContext = EX04_16.this;
-		// show a message of authentication is successful in first time
-		isShowTxt = true;
-		// check network setting on device
-		checkConnection();
-		// input decryption key
-		showPersonalKey();
+
+		startService(new Intent(
+				"trustedappframework.subprojecttwo.interfaces.InterfaceTest.loadMethod"));
+		startService(new Intent("irdc.ex04_16.EX04_16"));
 
 		/* 取得相關物件 */
 		mText = (TextView) findViewById(R.id.mText);
@@ -121,21 +106,24 @@ public class EX04_16 extends Activity {
 		/* 替mButton加入onClickListener，使其按下後三張牌都翻為背面且重新洗牌 */
 		mButton.setOnClickListener(new Button.OnClickListener() {
 			public void onClick(View v) {
-				mText.setText(getResources().getString(R.string.str_title));
-
-				mImageView01.setImageDrawable(getResources().getDrawable(
-						R.drawable.p04));
-				mImageView02.setImageDrawable(getResources().getDrawable(
-						R.drawable.p04));
-				mImageView03.setImageDrawable(getResources().getDrawable(
-						R.drawable.p04));
-				mImageView01.setAlpha(255);
-				mImageView02.setAlpha(255);
-				mImageView03.setAlpha(255);
-				randon();
-				choiceStatus = 0;
-
 				Log.i(TAG, "new");
+
+				myACAPD = new ACAPD(class_separation_segment2, personal_key2);
+
+				// mText.setText(getResources().getString(R.string.str_title));
+				//
+				// mImageView01.setImageDrawable(getResources().getDrawable(
+				// R.drawable.p04));
+				// mImageView02.setImageDrawable(getResources().getDrawable(
+				// R.drawable.p04));
+				// mImageView03.setImageDrawable(getResources().getDrawable(
+				// R.drawable.p04));
+				// mImageView01.setAlpha(255);
+				// mImageView02.setAlpha(255);
+				// mImageView03.setAlpha(255);
+				// randon();
+				// choiceStatus = 0;
+
 			}
 		});
 	}
@@ -146,81 +134,19 @@ public class EX04_16 extends Activity {
 		d02 = getResources().getDrawable(s1[1]);
 		d03 = getResources().getDrawable(s1[2]);
 
-		// ---check user and download encrypted Jar---
-		SendPostRunnable sr = new SendPostRunnable(fileName);
+		mI01 = mImg01;
+		mI02 = mImg02;
+		mI03 = mImg03;
+		mys1 = mysol01;
+		ans = R.drawable.p01;
+		choiceStatus = 1;
 
-		// start a Thread, the data to be transferred into the Runnable, so that
-		// Thread execute
-		Thread t = new Thread(sr);
-		t.start();
-
-		try {
-			// wait Thread t
-			t.join();
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			Log.e(TAG, e.getMessage());
-		}
-
-		if (sr.getResult()) {
-			if (isShowTxt)
-				Toast.makeText(
-						mAppContext,
-						getResources().getString(R.string.toast_checkuser_true),
-						Toast.LENGTH_SHORT).show();
-			// show a message of Authentication is successful in first time
-			isShowTxt = false;
-
-			mI01 = mImg01;
-			mI02 = mImg02;
-			mI03 = mImg03;
-			mys1 = mysol01;
-			ans = R.drawable.p01;
-			choiceStatus = 1;
-
-			if (new File(outputFilePath + fileName).exists()) {
-				Log.i(TAG, "Jar is exist");
-
-				// decrypt EB(session_key) -----
-				String session_key = Decrypt.decryptSessionKey(sr.getSession()
-						.get("s_enable_block"),
-						sr.getSession().get("s_enable_block2"), sr.getSession()
-								.get("s_enable_block3"), personal_key);
-				// System.out.println("session_key=" + session_key);
-				// String session_key="589040359334464";
-
-				// decrypt CB(Jar) -----
-				Decrypt decfile = new Decrypt(fileName, outputFilePath,
-						session_key);
-				decfile.decryptJar();
-
-				loadFileName = decfile.getOutputFileName();
-			}
-
-			if (new File(outputFilePath + loadFileName).exists()) {
-				// dynamic loading -----
-				Load ld = new Load(loadFileName, outputFilePath);
-				ld.loadJar();
-
-				sr = new SendPostRunnable(fileName, personal_key,
-						sr.getSession());
-				t = new Thread(sr);
-				t.start();
-			} else {
-				showPersonalKeyError("Load Error !");
-				Log.e(TAG, "Error: " + loadFileName);
-			}
-
-		} else {
-			// show a Alert Dialog that Authentication is failed
-			showCheckUserError();
-		}
+		myACAPD = new ACAPD(class_separation_segment, personal_key);
 
 	}
 
 	/* 重新洗牌的程式 */
-	private void randon() {
+	public static void randon() {
 		for (int i = 0; i < 3; i++) {
 			int tmp = s1[i];
 			int s = (int) (Math.random() * 2);
