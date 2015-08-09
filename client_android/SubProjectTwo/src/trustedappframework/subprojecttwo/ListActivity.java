0e4aa8a553b19206c17a956dfc9db011c2dbe4b1
jane0828 <janewu0828@gmail.com>
2015-08-09 23:52:58 +0800

    add JSON

--- client_android/SubProjectTwo/src/trustedappframework/subprojecttwo/ListActivity.java
+++ client_android/SubProjectTwo/src/trustedappframework/subprojecttwo/ListActivity.java
@@ -5,9 +5,24 @@ import static trustedappframework.subprojecttwo.module.ProjectConfig.checkConnec
 import static trustedappframework.subprojecttwo.module.ProjectConfig.mAppContext;
 import static trustedappframework.subprojecttwo.module.ProjectConfig.mContext;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -24,6 +39,7 @@ import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.os.StrictMode;
 import android.util.Log;
 import android.view.View;
 import android.view.Window;
@@ -35,6 +51,10 @@ import trustedappframework.subprojecttwo.module.ACAPDAsyncTask;
 
 public class ListActivity extends Activity {
 	private static final String TAG = "ListActivity";
+
+	String server_uri = ACAPDAsyncTask.appSecurityEnhancer_url
+			+ "/php/information.php";
+	ArrayList<HashMap<String, String>> arrList;
 
 	private ListView listView;
 	public static ProgressDialog progressDialog;
@@ -49,17 +69,53 @@ public class ListActivity extends Activity {
 	List<PackageInfo> pis;
 	List<ResolveInfo> appInfo;
 
-	ArrayList<HashMap<String, Object>> items = new ArrayList<HashMap<String, Object>>();
-
 	private void initACAPD() {
 		// get global Application object of the current process
 		mAppContext = getApplicationContext();
 		// get context for AlertDialog
 		mContext = ListActivity.this;
-		
+
 		ProgressDialog();
 		// check network setting on device
 		checkConnection();
+	}
+
+	private String getJsonData() {
+		StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+				.detectDiskReads().detectDiskWrites().detectNetwork() // or
+																		// .detectAll()
+																		// for
+																		// all
+																		// detectable
+																		// problems
+				.penaltyLog().build());
+
+		String str = "";
+		HttpResponse response;
+		HttpClient myClient = new DefaultHttpClient();
+		HttpPost myConnection = new HttpPost(server_uri);
+
+		List<BasicNameValuePair> pairs = new ArrayList<BasicNameValuePair>();
+		pairs.add(new BasicNameValuePair("UUID", ACAPDAsyncTask
+				.getUUID(mAppContext)));
+		try {
+			myConnection.setEntity(new UrlEncodedFormEntity(pairs, HTTP.UTF_8));
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+
+		try {
+			response = myClient.execute(myConnection);
+			str = EntityUtils.toString(response.getEntity(), "UTF-8");
+			System.out.println("-----------info-----------" + str);
+
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return str;
 	}
 
 	@Override
@@ -94,20 +150,47 @@ public class ListActivity extends Activity {
 							// (String) app.loadLabel(packageManager)));
 							// }
 
-							/** here **/
-							for (PackageInfo pi : pis) {
-								// / Only display non-system app info
-								if ((pi.applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) == 0
-										&& (pi.applicationInfo.flags & ApplicationInfo.FLAG_UPDATED_SYSTEM_APP) == 0) {
-									// // if non-system app info, add into
-									// applist
-									data.add(new Apps(pi.applicationInfo
-											.loadIcon(packageManager),
-											(String) pi.applicationInfo
-													.loadLabel(packageManager),
-											pi.applicationInfo.packageName));
+//							String json_str = getJsonData();
+//							try {
+//								JSONArray jArray = new JSONArray(json_str);
+//
+//								for (int i = 0; i < jArray.length(); i++) {
+//									JSONObject json = null;
+//									json = jArray.getJSONObject(i);
+//
+//									HashMap<String, String> map1 = new HashMap<String, String>();
+//
+//									// adding each child node to HashMap key =>
+//									// value
+//									map1.put("name", json.getString("name"));
+//
+//									// adding HashList to ArrayList
+//									arrList.add(map1);
+//								}
+//
+//							} catch (JSONException e) {
+//								e.printStackTrace();
+//							}
+//
+//							if (!arrList.isEmpty()) {
+//							Log.e(TAG,arrList+"");
+//						}
+								/** here **/
+								for (PackageInfo pi : pis) {
+									// / Only display non-system app info
+									if ((pi.applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) == 0
+											&& (pi.applicationInfo.flags & ApplicationInfo.FLAG_UPDATED_SYSTEM_APP) == 0) {
+										// // if non-system app info, add into
+										// applist
+										data.add(new Apps(
+												pi.applicationInfo
+														.loadIcon(packageManager),
+												(String) pi.applicationInfo
+														.loadLabel(packageManager),
+												pi.applicationInfo.packageName));
+									}
 								}
-							}
+//							}
 
 							msg.what = DATA_CREATLIST;
 						} else {
@@ -137,14 +220,14 @@ public class ListActivity extends Activity {
 						listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 							public void onItemClick(AdapterView<?> parent,
 									View view, int position, long id) {
-								
-								/**here**/
-								ACAPDAsyncTask task = new ACAPDAsyncTask(data.get(position)
-										.getPkg_name());
+
+								/** here **/
+								ACAPDAsyncTask task = new ACAPDAsyncTask(data
+										.get(position).getPkg_name());
 								task.execute((Void[]) null);
-//								ACAPD myACAPD = new ACAPD(data.get(position)
-//										.getPkg_name());
-//								myACAPD.ACAPD();
+								// ACAPD myACAPD = new ACAPD(data.get(position)
+								// .getPkg_name());
+								// myACAPD.ACAPD();
 							}
 						});
 
